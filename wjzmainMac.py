cc91d8a1c55db682ceccf9653e02cd2d8e118458
jie xu <jiexu@jiedeMacBook-Pro.local>
2017-01-30 16:34:02 +0800

    Initial commit

---
+++ wjzmainMac.py
@@ -0,0 +1,13 @@
+import time
+
+class catx:
+
+    def __init__(self):
+        print('haah,it is main entrance')
+
+    def catt(self):
+        print('hello')
+        time.sleep(1)
+
+c = catx()
+c.catt()
